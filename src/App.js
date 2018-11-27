d1bbbaee399bbebbaa77e7bf910870443d9da1bc
kyliesanderson <kyliesanderson@utexas.edu>
2018-11-27 14:02:12 -0600

    created rough navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Nav, NavItem } from 'react-bootstrap';
+import {Container } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container className="App">
+         <a href="/">
+            <h1> Austin Raye Haffelfinger</h1>
+         </a>
+         <Nav bsStyle="pills" justified className= "nav">
+            <NavItem href="/about">About</NavItem>
+            <NavItem href="/portfolio">Portfolio</NavItem>
+            <NavItem href="/resume">Resume</NavItem>
+         </Nav>
+      </Container>
     );
   }
+
 }
 
 export default App;
