0128e1ba505b4075622dd5f68991a34d2cdb39b8
kyliesanderson <kyliesanderson@utexas.edu>
2019-01-04 16:40:14 -0600

    finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Col, Row} from 'reactstrap';
+import {Col, Row, Container} from 'reactstrap';
 
 class About extends Component {
    constructor(props){
@@ -10,14 +10,10 @@ class About extends Component {
 
    render(){
       return (
-         <Row>
-            <Col xs="12" sm="12" md="7" lg="7">
+         <Container>
                <img className="about-img" src={require("../Images/cat.jpg")} />
-            </Col>
-            <Col xs="12" sm="12" md="5" lg="5">
                <p className="about-text">Lorem ipsum dolor sit amet, ei eam affert epicuri probatus, similique definiebas interpretaris nec in, soluta detracto eum et. Tale nostro nominavi ea usu, ut oblique gloriatur ullamcorper duo. Id tation habemus cum, est diam aliquip legimus et, sed verear ocurreret consulatu te. Mel id dolore possim legimus, solum menandri periculis per ad, duis vituperatoribus an eum. Unum efficiantur ut quo, eligendi signiferumque cu ius, at solet nostrud philosophia pro.</p>
-            </Col>
-         </Row>
+         </Container>
       );
    }
 }
